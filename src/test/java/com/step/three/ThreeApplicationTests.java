003b9703c69ec3359f17c1b874aacab7bdeb1d7c
Brad <Bradkharris@gmail.com>
2021-03-02 11:05:04 -0700

    firstish commit

---
+++ src/test/java/com/step/three/ThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.step.three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
